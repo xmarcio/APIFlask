4139caf97115102995b35ac924aff49d3c316720
Marcio Seefeld <marcioseefeld@gmail.com>
2022-10-25 10:46:39 -0300

    implementando login

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 api = Api(app)
+jwt = JWTManager(app)
 
-from .views import curso_views, formacao_views, professor_views
-from .models import curso_model, formacao_model, professor_model
+from .views import curso_views, formacao_views, professor_views, usuario_views, login_views
+from .models import curso_model, formacao_model, professor_model, usuario_model
