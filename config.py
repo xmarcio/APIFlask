7a8af0de89e45cf3bf602d6dd92fb9cef7b0409e
Marcio Seefeld <marcioseefeld@gmail.com>
2022-10-27 15:41:35 -0300

    db

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 DEBUG = True
 
-USERNAME = 'root'
-PASSWORD = 'Cloud99060127*'
-SERVER = 'localhost'
-DB = 'api_flask'
+#USERNAME = 'root'
+#PASSWORD = 'Cloud99060127*'
+#SERVER = 'localhost'
+#DB = 'api_flask'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://admin:5YJDHZ1B33B0URXGG1TyIDWYb1lwaWQv@jtllt1.stackhero-network.com:5432/admin?sslmode=require'
 #SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
 #SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SECRET_KEY = 'aplicacao_flask'
+#SECRET_KEY = 'aplicacao_flask'
